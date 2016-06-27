143da8a1b4380a5e27a09ac26c677fec94a90155
JoeE <JE4440@gmail.com>
2016-06-27 13:05:17 -0400

    added functionality to show how your custom xib will load

--- NibShortcut/CustomView.swift
+++ NibShortcut/CustomView.swift
@@ -9,6 +9,59 @@
 import UIKit
 
 class CustomView: UIView {
+    
+    typealias Frame = CGRect
+    
+    enum Message { case Welcome, Joke, Secret }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func back(sender: AnyObject) {
+        
+        UIView.animateWithDuration(0.33, animations: { 
+            self.alpha = 0
+            }) { (Bool) in
+                self.removeFromSuperview()
+        }
+        
+    }
+    
+    class func xibSetup(initWithMessage message: Message, frame:Frame) -> CustomView {
+        
+        guard let customView = CustomView.loadFromNibNamed("CustomView") as? CustomView else { return CustomView() }
+        
+        customView.initalize(message: message, frame: frame)
+        
+        return customView
+        
+    }
+    
+    private func initalize(message message: Message, frame:Frame) {
+        
+        alpha = 0
+        self.frame = frame
+        
+        switch message {
+        case .Welcome:
+            label.text = "Welcome to your custom view!"
+        case .Joke:
+            label.text = "Why is six afraid of seven? :)"
+        case .Secret:
+            label.text = "I open at the close" 
+            
+        }
+        
+        UIView.animateWithDuration(0.33) { self.alpha = 1 }
+        
+    }
 
+}
 
+extension UIView {
+    
+    class func loadFromNibNamed(nibNamed: String, bundle : NSBundle? = nil) -> UIView? {
+        return UINib(nibName: nibNamed, bundle: bundle).instantiateWithOwner(nil, options: nil)[0] as? UIView
+        
+    }
+    
 }
--- NibShortcut/ViewController.swift
+++ NibShortcut/ViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let messages:[CustomView.Message] = [ .Welcome, .Joke, .Secret ]
+    
+    var choice:Int = 0
+    
     @IBAction func loadView(sender: AnyObject) {
+        
+        print(messages.count)
+        
+        if choice == messages.count { choice = 0 }
+        
+        let customView = CustomView.xibSetup(initWithMessage: messages[choice], frame: view.frame)
+        view.addSubview(customView)
+        
+        choice += 1
         
     }
 
